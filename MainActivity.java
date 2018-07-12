0d169ecb0ce2c46e5e8155c962797c6af0d66c21
Julian Osborne <subaruforge@gmail.com>
2018-07-12 15:42:22 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,103 @@
 package tufts;
 
 import android.content.*;
+import android.graphics.*;
+import android.net.wifi.*;
 import android.os.*;
 import android.support.v7.app.*;
 import android.view.*;
 import android.widget.*;
 import fi.iki.elonen.*;
-import java.lang.reflect.*;
-import java.util.*;
-import thingworx.*;
+import java.io.*;
 import thingworx.tufts.*;
 
-import thingworx.Properties;
-import java.io.*;
-
 public class MainActivity extends AppCompatActivity {
- TextView resultTextView;
- Server s;
+	RelativeLayout canvas;
+	Server s;
+	Creator c = null;
+	TextView[] textviews = new TextView[100];
+	ImageView[] images = new ImageView[100];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-     resultTextView = (TextView) findViewById(R.id.result);
-  s = new Server();
+	    canvas = (RelativeLayout) findViewById(R.id.canvas);
+		s = new Server();
+		c = new Creator(canvas, MainActivity.this);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setTitle("Enable Web Server?");
+		dialog.setMessage("This will deploy a WebServer at http://"+phoneIp()+":8080/");
+		dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+			public void onClick(DialogInterface dialog, int id){
+				try {
+					s.start();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		dialog.setNegativeButton("No", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int id){
+					System.exit(0);
+				}
+			});
+			dialog.setCancelable(false);
+			dialog.show();
     }
- 
- private class Server extends NanoHTTPD {
-        public Server(){
+	
+	private class Server extends NanoHTTPD {
+		public Server(){
             super(8080);
         }
 
         @Override
-        public Response serve(String uri, Method method, Map<String, String> header, final Map<String, String> parameters, Map<String, String> files) {
-   if(parameters.get("value") != null){
-    runOnUiThread(new Runnable() {
-     @Override
-     public void run(){
-      resultTextView.setText("value=" + parameters.get("value"));
-     }
-    });
-   }
-      return newFixedLengthResponse("value=" + parameters.get("value"));
-  }
- }
- 
- public void start(View v) throws IOException{
-  s.start();
-  Toast.makeText(this, "Webserver running", Toast.LENGTH_SHORT).show();
- }
- 
- public void stop(View v){
-  s.stop();
-  Toast.makeText(this, "Webserver stopped", Toast.LENGTH_SHORT).show();
- }
+        public Response serve(final IHTTPSession session) {
+			final Request r = new Request(session);
+			final String text = r.getText();
+			final Float x = r.getX();
+			final Float y = r.getY();
+			final int id = r.getId();
+			final Float size = r.getSize();
+			final int[] color = r.getColor();
+			final String imageUrl = r.getImageUrl();
+			if(r.getParam("id") != null){
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run(){
+						if(imageUrl != null){
+							if(r.isAdd()){
+								c.addImage(imageUrl, x, y, id);
+							}
+							if(r.isUpdate()){
+								c.updateImage(imageUrl, x, y, id);
+							}
+							if(r.isDelete()){
+								c.deleteImage(id);
+							}
+						} else {
+							if(r.isAdd()){
+								c.addText(text, x, y, id, size, color);
+							}
+							if(r.isUpdate()){
+								c.updateText(text, x, y, id, size, color);
+							}
+							if(r.isDelete()){
+								c.deleteText(id);
+							}
+						}
+					}
+				});
+			}
+    		return newFixedLengthResponse("Connected.");
+		}
+	}
+	
+	public String phoneIp(){
+		WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+		int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+		final String formattedIpAddress = String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+		return formattedIpAddress;
+	}
 }
+
